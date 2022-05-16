cec88042449436ec211bbf38b61f9085cbde1646
Edward_Andino <edjandinos@gmail.com>
2022-05-15 23:03:32 -0500

    repo created and push for the 1st time

---
+++ src/main/java/Email_parser.java
@@ -0,0 +1,99 @@
+import com.pff.PSTException;
+import com.pff.PSTFile;
+import com.pff.PSTFolder;
+import com.pff.PSTMessage;
+
+import java.io.IOException;
+import java.util.Vector;
+
+import static java.lang.System.out;
+
+public class Email_parser {
+
+    public static void main(String[] args) {
+        String pstFile = "C:\\Users\\edjan\\Desktop\\outlook.pst"; //set pstFile into args[1]
+        String o = parseEmail(pstFile);
+        out.println(o);
+    }
+
+    public static String parseEmail(String fileName) {
+        try {
+            PSTFile pstFile = new PSTFile(fileName);
+            out.println(pstFile.getMessageStore().getDisplayName());
+            processFolder(pstFile.getRootFolder());
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+        return fileName;
+    }
+
+    public static int depth = -1;
+
+    public static void processFolder(PSTFolder folder)
+            throws PSTException, IOException
+    {
+        depth++;
+        // the root folder doesn't have a display name
+        if (depth > 0) {
+            //printDepth();
+            out.println(folder.getDisplayName());
+        }
+
+        // go through the folders...
+        if (folder.hasSubfolders()) {
+            Vector<PSTFolder> childFolders = folder.getSubFolders();
+            for (PSTFolder childFolder : childFolders) {
+                processFolder(childFolder);
+            }
+        }
+
+        // and now the emails for this folder
+        if (folder.getContentCount() > 0) {
+
+            depth++;
+            PSTMessage email = null;
+            try {
+                email = (PSTMessage)folder.getNextChild();
+            } catch (PSTException e) {
+                e.printStackTrace();
+            }
+            int emailNumber = 0;
+            while (email != null) {
+                emailNumber++;
+                printDepth();
+                out.println("\n****NEXT EMAIL******"+emailNumber);
+                out.println("Email Sender Name: " + email.getSenderName() +"["+ email.getSenderEmailAddress()+"]");
+                out.println("Email Received Name: " + email.getReceivedByName() +"["+  email.getReceivedByAddress()+"]");
+                out.println("Email Subject: " + email.getSubject());
+                out.println("Email Time: " + email.getMessageDeliveryTime());
+                //out.println("Email has attachment?: " + email.hasAttachments());
+                out.println("Email has Replied: " + email.hasReplied());
+                if(email.hasReplied()){
+                    out.println("\n****EMAIL BODY******");
+                    out.println(email.getBody());
+                    getRepliedEmails(email.getBody());
+                }
+                if(email.hasForwarded()){
+                    getForwardedEmails(email.getBody());
+                }
+                email = (PSTMessage)folder.getNextChild();
+            }
+            depth--;
+        }
+        depth--;
+    }
+    public static String getRepliedEmails(String body){
+        return body;
+    }
+
+    public static String getForwardedEmails(String body){
+        return body;
+    }
+
+    public static void printDepth() {
+        for (int x = 0; x < depth-1; x++) {
+            out.print(" | ");
+        }
+        out.print(" |- ");
+    }
+}
---
+++ src/main/java/Forensics_Tool.java
@@ -0,0 +1,81 @@
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class Forensics_Tool {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        String runCommand;
+        switch (args[0]) {
+            case "-i" -> {
+                runCommand = "exiftool.exe ";
+                out.println(getCommand(runCommand + args[1]));
+            }
+            case "-d" -> {
+                runCommand = String.format("RECmd.exe -f \"%s\"\\SYSTEM.DAT --kn \"ControlSet001\\Control\\ComputerName\\ComputerName\" --nl", args[1]);
+                String getSystem_InfoName = getCommand(runCommand);
+                out.println("Computer Name:\n" + getSystem_InfoName);
+
+                String runCommand2 = String.format("RECmd.exe -f \"%s\"\\NTUSER.DAT --kn \"Software\\Microsoft\\Windows NT\\CurrentVersion\\Time Zones\" --nl", args[1]);
+                String getSystem_InfoTime = getCommand(runCommand2);
+                out.println("Time zone:\n" + getSystem_InfoTime);
+            }
+            case "-r" -> {
+                //C:\Users\edjan\Desktop\NTUSER.DAT;
+                runCommand = String.format("RECmd.exe -f \"%s\"\\NTUSER.DAT --kn \"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ComDlg32\\OpenSaveMRU\\*\" --nl --sa --bn", args[1]);
+                out.println(getCommand(runCommand));
+            }
+            case "-u" -> {
+                //C:\Users\edjan\Desktop\NTUSER.DAT;
+                runCommand = String.format("RECmd.exe -f \"%s\"\\NTUSER.DAT --kn \"Software\\Microsoft\\Internet Explorer\\TypedURLs\" --nl --sa --bn", args[1]);
+                out.println(getData(getCommand(runCommand), "Data:"));
+            }
+            case "-e" -> out.println("run Email_parser");
+            //Email_parser.parseEmail(args[1]);//throws errors
+            case "-h" -> printError();
+            default -> {
+                out.println("Argument not found: -help for more information");
+                printError();
+            }
+        }
+    }//end main
+
+    public static void printError(){
+        out.println("\nNote:\n");
+        out.println("Usage: Forensics_Tool [options] [registry path]\n");
+        out.println("Example: java Forensics_Tool -d C:\\Users\\UserOne\\Desktop\\My_Registries");
+        out.println("Options:");
+        out.println("-i     Display the info of the image (include the file name and extension. eg: hello.txt)");
+        out.println("-d     Display the info of the system");
+        out.println("-r     Retrieve recent open files and recent access documents");
+        out.println("-u     Retrieve typed URLS");
+        out.println("-e     parse email");
+    }
+    public static String getData(String line, String word) {
+        String[] arrayToSplit = line.split("\s");
+        String[] values = new String[arrayToSplit.length];
+
+        String getData = "";
+        for(int i = 0; i < arrayToSplit.length; i++){
+            if(arrayToSplit[i].contains(word)){
+                getData = arrayToSplit[i+1];
+            }
+             values[i] = getData;
+        }
+        //remove duplicates
+        values = Arrays.stream(values).distinct().toArray(String[]::new);
+
+        return String.join("\n ",values);
+    }
+
+    /*
+     *executes a windows command
+     */
+    public static String getCommand(String cmd) throws IOException {
+        Scanner s = new Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }//end getCommand method
+
+}
